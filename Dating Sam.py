8df869ae37d8eade447e6b8e1f40dfd81d7e23d0
miyuzett <monserratt.nincolaf@estudiantes.colegiotgs.cl>
2025-10-27 15:31:28 -0300

    Update Dating Sam.py

--- Dating Sam.py
+++ Dating Sam.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 
 
@@ -8,7 +7,13 @@ col1, col2 = st.columns(2)
 col1.write("En esta pagina tendras el privilegio de ver que tan compatible eres con el **pequeño sam** :heart:")
 col2.write("si no has tenido pareja por años y años esta es tu solucion, tendras que contestar una serie de preguntas, que decidiran tu futuro con el niño sam")
 col1.write("cada pregunta es importante así que **responde con mucho cuidado**")
+st.header("¿quienes somos?")
+
+st.write(" nosotras somos simplemente aficionad@s del famoso niño sam y solo queremos hacer algo de humor con su persona")
 
 col2.write("el famoso niño sam recientemente cumplio los 300 años de edad y lamentablemente los a pasado todos solo, por lo tanto tu seras el/la responsable de ver tu compatibilidad con el pequeño sam y ganar su amor")
+
+
+
 
 
