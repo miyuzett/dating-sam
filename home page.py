8269b700991ba55b169e32392616194329c60191
miyuzett <monserratt.nincolaf@estudiantes.colegiotgs.cl>
2025-10-27 15:47:20 -0300

    Update home page.py

--- home page.py
+++ home page.py
@@ -9,15 +9,12 @@ st.header("¿para que publico esta dirigida esta pagina?")
 
 st.write("esta dirigido a personas realmente interesadas en sam o simplemente curiosas por su historia, mas o menos a gente de 14+")
 
-st.header("¿quienes somos?")
-
-st.write(" nosotras somos simplemente aficionad@s del famoso niño sam y solo queremos hacer algo de humor con su persona")
-
 st.header("¿tenemos encerrado al niño sam?")
 
 st.write("si, esto siempre nos lo preguntan pero es hora de decir la verdad, realmente si tenemos al niño sam encerrado para nuestro beneficio monetario, __por si acaso es broma__")
 
 st.write("bueno sin mas que decir les dejamos a continuacion por si tienen mas preguntas, consultar al numero: ")
+
 
 
 
