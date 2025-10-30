5adce861ed0ae89a5ede6d7c7ecef8e8b28f4abb
miyuzett <monserratt.nincolaf@estudiantes.colegiotgs.cl>
2025-10-30 14:35:44 -0300

    Update home page.py

--- home page.py
+++ home page.py
@@ -17,15 +17,19 @@ col1.header("¿para que publico esta dirigida esta pagina?")
 
 col1.write("esta dirigido a personas realmente interesadas en sam o simplemente curiosas por su historia, mas o menos a gente de 14+")
 
-col1.header("¿tenemos encerrado al niño sam?")
+col1.header("¿de donde sacamos la idea de la app de citas?")
 
 
 #texto
-col1.write("si, esto siempre nos lo preguntan pero es hora de decir la verdad, realmente si tenemos al niño sam encerrado para nuestro beneficio monetario, __por si acaso es broma__")
-
+col1.write("sacamos la idea de un viejo juego de app de citas de Justin Bieber el cual tiene la mecanica de dar un puntaje al final, nosotras quisimos recrear eso pero con el niño sam y preguntas personalizadas sobre el ")
+col1.image("Captura de pantalla 2025-10-20 132425.png")
 col1.write("bueno sin mas que decir les dejamos a continuacion por si tienen mas preguntas, consultar al numero: ")
 
 col2.image("Captura de pantalla 2025-09-01 133531.png")
+ 
+
+
+
 
 
 
