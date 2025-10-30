f609a35f51514a2f81291a1fe2108d0a1b03a8be
miyuzett <monserratt.nincolaf@estudiantes.colegiotgs.cl>
2025-10-30 13:41:09 -0300

    Update home page.py

--- home page.py
+++ home page.py
@@ -4,24 +4,29 @@ import streamlit as st
 #titulo
 st.title("**se conocedor de la historia detras**")
 
+
+col1, col2 = st.columns(2)
 #subtitulo
-st.header("¿quien es el niño sam?")
+col1.header("¿quien es el niño sam?")
 
 #texto
-st.write("el niño sam es un carismatico niño de una aplicacion para programar de manera facil y rapida llamada scratch, y desde el momento en el que lo conocimos no hemos parado de usarlo en nuestros diseños y programas") 
+col1.write("el niño sam es un carismatico niño de una aplicacion para programar de manera facil y rapida llamada scratch, y desde el momento en el que lo conocimos no hemos parado de usarlo en nuestros diseños y programas") 
 
 #subtitulo
-st.header("¿para que publico esta dirigida esta pagina?")
+col1.header("¿para que publico esta dirigida esta pagina?")
 
-st.write("esta dirigido a personas realmente interesadas en sam o simplemente curiosas por su historia, mas o menos a gente de 14+")
+col1.write("esta dirigido a personas realmente interesadas en sam o simplemente curiosas por su historia, mas o menos a gente de 14+")
 
-st.header("¿tenemos encerrado al niño sam?")
+col1.header("¿tenemos encerrado al niño sam?")
 
 
 #texto
-st.write("si, esto siempre nos lo preguntan pero es hora de decir la verdad, realmente si tenemos al niño sam encerrado para nuestro beneficio monetario, __por si acaso es broma__")
+col1.write("si, esto siempre nos lo preguntan pero es hora de decir la verdad, realmente si tenemos al niño sam encerrado para nuestro beneficio monetario, __por si acaso es broma__")
 
-st.write("bueno sin mas que decir les dejamos a continuacion por si tienen mas preguntas, consultar al numero: ")
+col1.write("bueno sin mas que decir les dejamos a continuacion por si tienen mas preguntas, consultar al numero: ")
+
+col2.image("Captura de pantalla 2025-09-01 133531.png")
+
 
 
 
