e75692e9dd9313ba5ce3d678087bf35fda288852
miyuzett <monserratt.nincolaf@estudiantes.colegiotgs.cl>
2025-10-30 13:33:25 -0300

    Update home page.py

--- home page.py
+++ home page.py
@@ -1,19 +1,28 @@
+#librerias
 import streamlit as st
 
+#titulo
 st.title("**se conocedor de la historia detras**")
+
+#subtitulo
 st.header("¿quien es el niño sam?")
 
+#texto
 st.write("el niño sam es un carismatico niño de una aplicacion para programar de manera facil y rapida llamada scratch, y desde el momento en el que lo conocimos no hemos parado de usarlo en nuestros diseños y programas") 
 
+#subtitulo
 st.header("¿para que publico esta dirigida esta pagina?")
 
 st.write("esta dirigido a personas realmente interesadas en sam o simplemente curiosas por su historia, mas o menos a gente de 14+")
 
 st.header("¿tenemos encerrado al niño sam?")
 
+
+#texto
 st.write("si, esto siempre nos lo preguntan pero es hora de decir la verdad, realmente si tenemos al niño sam encerrado para nuestro beneficio monetario, __por si acaso es broma__")
 
 st.write("bueno sin mas que decir les dejamos a continuacion por si tienen mas preguntas, consultar al numero: ")
+
 
 
 
