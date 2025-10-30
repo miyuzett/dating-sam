e91db57d08cb2c2e38899f23572cdb06ac607ded
miyuzett <monserratt.nincolaf@estudiantes.colegiotgs.cl>
2025-10-30 13:28:29 -0300

    Update Dating Sam.py

--- Dating Sam.py
+++ Dating Sam.py
@@ -1,9 +1,12 @@
-import streamlit as st
+
 import streamlit
-st.title("_Dating Sam_:heart_eyes:")
-st.header("¿eres un verdadero fan del niño sam?:kissing_heart:")
-col1, col2 = st.columns(2)
-col1.write("En esta pagina tendras el privilegio de ver que tan compatible eres con el **pequeño sam** :heart:")
+import streamlit as st
+
+st.title("_Dating Sam_:heart_eyes:") #titulo
+st.header("¿eres un verdadero fan del niño sam?:kissing_heart:") #subtitulo
+col1, col2 = st.columns(2) #marca columnas
+#texto
+col1.write("En esta pagina tendras el privilegio de ver que tan compatible eres con el **pequeño sam** :heart:") 
 col2.write("si no has tenido pareja por años y años esta es tu solucion, tendras que contestar una serie de preguntas, que decidiran tu futuro con el niño sam")
 col1.write("cada pregunta es importante así que **responde con mucho cuidado**")
 
@@ -11,12 +14,16 @@ col2.write("el famoso niño sam recientemente cumplio los 300 años de edad y la
 
 col1.image("1000046784.jpg")
 
-
+#titulo
 col2.header("¿quienes somos?")
+
+
+#texto
 col2.write(" nosotras somos simplemente aficionad@s del famoso niño sam y solo queremos hacer algo de humor con su persona, se pueden referir a nosotr@s como **Azharx y miyuzett**")
 col2.write("bueno sin mas que decir les dejamos a continuacion por si tienen mas preguntas nuestro siguiente formato: ")
 col2.write(" gmail: contacto@mega100.com")
 col2.write("numero: +1 (555) 234-9876")
+
 
 
 
